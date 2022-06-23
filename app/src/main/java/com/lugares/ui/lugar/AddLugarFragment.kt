d0b99da535f6606620be47f7190de2cf7558dafd
jenny052001 <jennyar05@hotmail.com>
2022-06-22 19:26:02 -0600

    Version Semana 5

--- app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
@@ -1,13 +1,21 @@
 package com.lugares.ui.lugar
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationAvailability
+import com.google.android.gms.location.LocationServices
 import com.lugares.R
 import com.lugares.databinding.FragmentAddLugarBinding
 import com.lugares.databinding.FragmentLugarBinding
@@ -31,22 +39,62 @@ class AddLugarFragment : Fragment() {
 
         _binding = FragmentAddLugarBinding.inflate(inflater, container, false)
 
-        binding.btAgregar.setOnClickListener{
+        binding.btAgregar.setOnClickListener {
             addLugar()
         }
-
+        ubicaGPS() //activa los permisos para el GPS y muestra la info
 
 
         return binding.root
     }
+
+    //
+    private fun ubicaGPS() {
+        val ubicacion: FusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+        if (ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+            &&
+            ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {  //Si no tengo los permisos entonces los solicito
+            ActivityCompat.requestPermissions(requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION),105)
+        } else {  //Si se tienen los permisos
+            ubicacion.lastLocation.addOnSuccessListener { location: Location? ->
+                if (location!=null) {
+                    binding.tvLatitud.text ="${location.latitude}"
+                    binding.tvLongitud.text ="${location.longitude}"
+                    binding.tvAltura.text ="${location.altitude}"
+                } else {
+                    binding.tvLatitud.text ="0.00"
+                    binding.tvLongitud.text ="0.00"
+                    binding.tvAltura.text ="0.00"
+                }
+            }
+        }
+    }
+
+
+
+
+
+
 
     private fun addLugar() {
         val nombre = binding.etNombre.text.toString()
         val correo = binding.etCorreo.text.toString()
         val telefono = binding.etTelefono.text.toString()
         val web = binding.etWeb.text.toString()
+        val latitud = binding.tvLatitud.text.toString().toDouble()
+        val longitud = binding.tvLongitud.text.toString().toDouble()
+        val altura = binding.tvAltura.text.toString().toDouble()
+
         if (nombre.isNotEmpty()) {
-            val lugar = Lugar(0, nombre, correo, telefono, web, 0.0, 0.0, 0.0, "", "",)
+            val lugar = Lugar(0, nombre, correo, telefono, web,latitud, longitud, altura, "", "",)
             lugarViewModel.addLugar(lugar)
             Toast.makeText(requireContext(),getString(R.string.msg_lugar_added),Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_addLugarFragment_to_nav_lugar) // esta funcion nos envia a la pantalla principal
--- app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -1,10 +1,13 @@
 package com.lugares.ui.lugar
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -38,19 +41,113 @@ class UpdateLugarFragment : Fragment() {
         _binding = FragmentUpdateLugarBinding.inflate(inflater, container, false)
 
         // Coloco la info del lugar en los campos del fragmento para modificar.
-        binding.etNombre.setText(args.lugar.nombre)
-        binding.etCorreo.setText(args.lugar.correo)
-        binding.etTelefono.setText(args.lugar.telefono)
-        binding.etWeb.setText(args.lugar.Web)
+        binding.run {
+            etNombre.setText(args.lugar.nombre)
+            etCorreo.setText(args.lugar.correo)
+            etTelefono.setText(args.lugar.telefono)
+            etWeb.setText(args.lugar.Web)
+            binding.tvAltura.text = args.lugar.altura.toString()
+            binding.tvLatitud.text = args.lugar.latitud.toString()
+            binding.tvLongitud.text = args.lugar.longitud.toString()
+
+            binding.btActualizar.setOnClickListener { UpdateLugar() }
+            binding.btEmail.setOnClickListener { escribirCorreo() }
+            binding.btPhone.setOnClickListener { realizarLlamada() }
+            binding.btPhone.setOnClickListener { verWeb() }
 
 
-        binding.btActualizar.setOnClickListener{
-            UpdateLugar()
+
+            binding.btActualizar.setOnClickListener {
+                UpdateLugar()
+            }
+            // Se indica que esta pantalla tiene un menu personalizado
+            setHasOptionsMenu(true)
+
+            return binding.root
+
         }
 
+    }
+    private fun verWeb() {
+        val recurso = binding.etWeb.text.toString()
+        if (recurso.isNotEmpty()) {
+           val accion =Intent(Intent.ACTION_VIEW,Uri.parse("hhttp:/$recurso"))
+            startActivity(accion) // Carga el app de correo
+
+        } else {
+            Toast.makeText(
+                requireContext(), getString(R.string.msg_datos), Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun realizarLlamada(){
+            val recurso = binding.etTelefono.text.toString()
+            if (recurso.isNotEmpty()) {
+                val accion = Intent(Intent.ACTION_CALL) // para hacer la llamada
+                accion.data = Uri.parse("tel:$recurso")//nombre o telefono que le di en "recurso"
+                //validar si tiene permisos para hacer la llamada
+                if(requireActivity().checkSelfPermission(Manifest.permission.CALL_PHONE)!=
+                    PackageManager.PERMISSION_GRANTED){ // PackageManager saber si el permiso de la llamda se ha otorgado.
+               // sino se ha otorgado el permiso de hacer llamadas, se pide el permiso
+                    requireActivity().requestPermissions(arrayOf(Manifest.permission.CALL_PHONE),105) // aparece un mensaje de permisos para hacer la llamada.
+
+                }else { // si se tiene permisos se hace la llamada
+
+                requireActivity().startActivity(accion) // metodo para levantar la llamada
+                }
+
+            } else {
+                Toast.makeText(
+                    requireContext(), getString(R.string.msg_datos), Toast.LENGTH_SHORT).show()
+            }
+    }
 
 
-        return binding.root
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Consulta si se dio click en el icono borrar
+        if(item.itemId==R.id.menu_delete){
+            deleteLugar()
+
+        }
+        return super.onOptionsItemSelected(item)
+
+
+    }
+    private fun deleteLugar(){
+        val pantalla = AlertDialog.Builder(requireContext())
+        pantalla.setTitle(R.string.delete)
+
+        pantalla.setMessage(getString(R.string.seguroBorrar)+"${args.lugar.nombre}?")
+
+        pantalla.setPositiveButton(getString((R.string.si))) {_,_ ->
+            lugarViewModel.deletelugar(args.lugar)
+            findNavController().navigate(R.id.action_nav_lugar_to_UpdateLugarFragment)
+        }
+        pantalla.setNegativeButton(getString(R.string.no)) {_,_ ->}
+        pantalla.create().show()
+
+    }
+        private fun escribirCorreo() {
+        val recurso = binding.etCorreo.text.toString()
+        if (recurso.isNotEmpty()) {
+            val accion = Intent(Intent.ACTION_SEND)
+            accion.type = "message/rfc822" // esto es un correo electronico
+            accion.putExtra(Intent.EXTRA_EMAIL, arrayOf(recurso))
+            accion.putExtra(
+                Intent.EXTRA_SUBJECT,
+                getString(R.string.msg_saludos) + " " + binding.etCorreo.text)
+            accion.putExtra(Intent.EXTRA_TEXT, getString(R.string.msg_mensaje_correo))
+            startActivity(accion) // Carga el app de correo
+
+        } else {
+            Toast.makeText(
+                requireContext(), getString(R.string.msg_datos), Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     private fun UpdateLugar() {
@@ -77,3 +174,6 @@ class UpdateLugarFragment : Fragment() {
         _binding = null
     }
 }
+
+
+
